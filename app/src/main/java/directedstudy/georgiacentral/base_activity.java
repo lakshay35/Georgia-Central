b82641b812cd7a62199ec9114c1771fa509383c0
lakshaysharma <lakshay35@gmail.com>
2018-01-09 17:58:36 -0500

    Initial Commit

---
+++ app/src/main/java/directedstudy/georgiacentral/base_activity.java
@@ -0,0 +1,13 @@
+package directedstudy.georgiacentral;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class base_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_activity);
+    }
+}
