78ccbd6c6bf82af071cb310080d2cb386fd55446
lakshaysharma <lakshay35@gmail.com>
2018-01-10 13:32:16 -0500

    Added Navigator Activity

---
+++ app/src/main/java/directedstudy/georgiacentral/Login.java
@@ -0,0 +1,21 @@
+package directedstudy.georgiacentral;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, Navigator.class);
+        intent.putExtra("go", "go");
+        startActivity(intent);
+    }
+}
